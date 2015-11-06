f6bac4b121b4447c49b0c6b1c233cf3f99c0b59a
Samir <samir.wdhw@gmail.com>
2015-11-07 04:07:59 +0530

    Final Commit

--- Gamesep.cpp
+++ Gamesep.cpp
@@ -1,5 +1,4 @@
 #include<simplecpp>
-#include<conio.h>
 #include"gamesep.h"
 #define pi 3.14159265358979323846264338327950288419716939937510
 #define r_2 1.41421356237309504880168872420969807856967187537694807317667973799
@@ -653,7 +652,7 @@ else if(  x<=185 && x>=115 && y>=295 && y<=305 )
     t1.reset(150, 230, "SPECIAL THANKS TO ADITYA KUMAR AKASH :) ");
     t2.reset(150, 250, "CREATED BY: ");
     t3.reset(150, 270, "Samir Wadhwa (150100024)");
-    t4.reset(150, 290, "Anish Ram Senati (150100023)");
+    t4.reset(150, 290, "Anish Ram Senathi (150100023)");
     getClick();
 }
 
